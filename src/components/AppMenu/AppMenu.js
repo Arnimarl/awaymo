ba3fe3811c46c905f35a4e4f31d8ec6ad6cfa1a0
Lukas <lukaszslomsk13@gmail.com>
2019-09-23 15:56:29 +0200

    App Menu component starter

---
+++ src/components/AppMenu/AppMenu.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class AppMenu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return ( 
+      <nav>
+        I am App Menu!
+      </nav>
+     );
+  }
+}
+ 
+export default AppMenu;
