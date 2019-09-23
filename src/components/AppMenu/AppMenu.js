7d3df66247a9789aff06b59fda7bcc57b01b9dbe
Lukas <lukaszslomsk13@gmail.com>
2019-09-23 21:18:48 +0200

    create desktop app menu

--- src/components/AppMenu/AppMenu.js
+++ src/components/AppMenu/AppMenu.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './app-menu.css';
 
 class AppMenu extends Component {
   constructor(props) {
@@ -7,8 +8,54 @@ class AppMenu extends Component {
   }
   render() { 
     return ( 
-      <nav>
-        I am App Menu!
+      <nav className="app-menu">
+        <div className="app-menu__wrapper">
+
+          <header className="app-menu__header">
+            <img src="/awaymoFullWhite.svg" />
+          </header>
+
+          <main className="app-menu__main">
+            <div>
+              <ul className="app-menu__list">
+                <li><a className="app-menu__list-link" href="">Home</a></li>
+                <li><a className="app-menu__list-link" href="">Flights</a></li>
+                <li><a className="app-menu__list-link" href="">About Us</a></li>
+                <li><a className="app-menu__list-link" href="https://intercom.help/awaymo" target="_blank">FAQ</a></li>
+                <li><a className="app-menu__list-link" href="https://intercom.help/awaymo" target="_blank">Support</a></li>
+                <li><a className="app-menu__list-link" href="">Contact Us</a></li>
+              </ul>
+            </div>
+
+            <div>
+              <div className="app-menu__user">
+                <div className="app-menu__user-avatar">
+                  <img src="/avatar.png" />
+                </div>
+
+                <div className="app-menu__user-info">
+                  <p>Dominik Biel</p>
+                  <p>£1,500.00 Available</p>
+                </div>
+              </div>
+
+              <ul className="app-menu__list">
+                <li><a className="app-menu__list-link" href="">Profile</a></li>
+                <li><a className="app-menu__list-link" href="">My Bookings</a></li>
+                <li><a className="app-menu__list-link" href="">My Payments</a></li>
+                <li><a className="app-menu__list-link" href="">Log Out</a></li>
+                <li><a className="app-menu__list-link" href="">Resume Application</a></li>
+              </ul>
+            </div>
+          </main>
+
+          <footer className="app-menu__footer">
+            <p>We're here to help</p>
+            <p>+44 (0) 20 8050 3459</p>
+            <p>support@awaymo.com</p>
+          </footer>
+
+        </div>
       </nav>
      );
   }
