a4adee36192b9206a37129337a66b1e4d2b00076
Lukas <lukaszslomsk13@gmail.com>
2019-09-25 20:49:25 +0200

    style for mobile add Lato

--- src/components/AppMenu/MenuMain.js
+++ src/components/AppMenu/MenuMain.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MenuMain = () => {
   return ( 
     <main className="app-menu__main">
-      <div>
+      <div className="mobile-hide">
         <ul className="app-menu__list">
           <li><a className="app-menu__list-link" href="">Home</a></li>
           <li><a className="app-menu__list-link" href="">Flights</a></li>
@@ -14,7 +14,7 @@ const MenuMain = () => {
         </ul>
       </div>
 
-      <div>
+      <div className="mobile-full">
         <div className="app-menu__user">
           <div className="app-menu__user-avatar">
             <img src="/avatar.png" />
@@ -22,16 +22,22 @@ const MenuMain = () => {
 
           <div className="app-menu__user-info">
             <p>Dominik Biel</p>
-            <p>£1,500.00 Available</p>
+            <p className="mobile-hide">£1,500.00 Available</p>
+            <p className="desktop-hide">Available Balance</p>
+            <p className="desktop-hide">£1,500.00</p>
           </div>
         </div>
 
         <ul className="app-menu__list">
-          <li><a className="app-menu__list-link" href="">Profile</a></li>
+          <li><a className="app-menu__list-link app-menu__list-link--first" href="">Profile</a></li>
           <li><a className="app-menu__list-link" href="">My Bookings</a></li>
           <li><a className="app-menu__list-link" href="">My Payments</a></li>
+          <li><a className="app-menu__list-link desktop-hide" href="">Support</a></li>
+          <li><a className="app-menu__list-link desktop-hide" href="">Contact Us</a></li>
           <li><a className="app-menu__list-link" href="">Log Out</a></li>
-          <li><a className="app-menu__list-link" href="">Resume Application</a></li>
+          <li><a className="app-menu__list-link mobile-hide" href="">Resume Application</a></li>
+          <li><a className="app-menu__list-link desktop-hide" href="">About</a></li>
+          <li><a className="app-menu__list-link app-menu__list-link--last desktop-hide" href="">FAQ</a></li>
         </ul>
       </div>
     </main>
