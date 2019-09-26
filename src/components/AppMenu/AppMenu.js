546173c42d387920b1b1dbdfcad99cd18677bf27
Lukas <lukaszslomsk13@gmail.com>
2019-09-26 02:01:08 +0200

    apply open/hide menu with animation

--- src/components/AppMenu/AppMenu.js
+++ src/components/AppMenu/AppMenu.js
@@ -4,24 +4,32 @@ import './app-menu-mobile.css';
 import MenuHeader from './MenuHeader';
 import MenuMain from './MenuMain';
 import MenuFooter from './MenuFooter';
+import { CSSTransition } from 'react-transition-group';
 
 class AppMenu extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = { }
   }
 
   render() { 
-    return ( 
-      <nav className="app-menu">
-        <div className="app-menu__wrapper">
+    return (
+      <CSSTransition
+        in={this.props.showMenu}
+        timeout={300}
+        classNames="app-menu"
+        unmountOnExit
+      >
+        <nav className="app-menu">
+          <div className="app-menu__wrapper">
 
-          <MenuHeader />
-          <MenuMain />
-          <MenuFooter />
+            <MenuHeader closeMenu={this.props.closeMenu}/>
+            <MenuMain />
+            <MenuFooter />
 
-        </div>
-      </nav>
+          </div>
+        </nav>
+      </CSSTransition>
      );
   }
 }
