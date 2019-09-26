93ad35e29de60ea5ae0f149bad93a5b70db8b17c
Lukas <lukaszslomsk13@gmail.com>
2019-09-27 00:08:22 +0200

    move svg

--- src/components/AppMenu/MenuHeader.js
+++ src/components/AppMenu/MenuHeader.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import LogoSvg from '../../assets/logo-svg';
 
 const MenuHeader = (props) => {
   return ( 
     <header className="app-menu__header">
-      <img src="/awaymoFullWhite.svg" />
+      <LogoSvg />
+
       <span onClick={() => {props.closeMenu()}} className="app-menu__close">
         <FontAwesomeIcon icon={faTimes} />
       </span>
