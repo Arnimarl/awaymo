4e1f30a6ee934eddf2237499cd39d93611db0f71
Lukas <lukaszslomsk13@gmail.com>
2019-09-26 23:24:15 +0200

    Updates

---
+++ precache-manifest.da975366bd88705263d2c7c8f8797e36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c38715b74b76433a9f39c98856150447",
+    "url": "/nazwa/index.html"
+  },
+  {
+    "revision": "ea2aa1d51cda042fb24e",
+    "url": "/nazwa/static/css/main.eaa3d9a5.chunk.css"
+  },
+  {
+    "revision": "1436fb689d8f03deec7c",
+    "url": "/nazwa/static/js/2.ac66461c.chunk.js"
+  },
+  {
+    "revision": "ea2aa1d51cda042fb24e",
+    "url": "/nazwa/static/js/main.0aabf87d.chunk.js"
+  },
+  {
+    "revision": "52e0c372864d619aa40f",
+    "url": "/nazwa/static/js/runtime~main.e23c9a83.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/nazwa/static/media/logo.ee7cd8ed.svg"
+  }
+]);
